99cf6cc89d1ed6ec49cfedb4bd285523531d99ab
Laura <laura.s.chavez@outlook.com>
2018-08-30 17:04:42 -0400

    draft aug30

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'users/new'
+  get 'users/show'
+  get 'user/new'
+  get 'user/show'
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
