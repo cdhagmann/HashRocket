290502e11561f15faf822b0a1d3d12f2db1dc187
josephrfarmer <josephr.farmer24@gmail.com>
2018-08-31 10:18:03 -0400

    crud methods started in comments controller
    pushing for team

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,32 @@
 class CommentsController < ApplicationController
+  def new 
+    @comment = Comment.new 
+  end 
+
+  def create 
+    @comment = Comment.new(comment_params) 
+
+    if @comment.save
+      redirect_to post_path, notice: 'Comment was successfully created.'
+    else
+      render :new 
+    end
+  end
+
+  def edit 
+  end  
+
+  def update 
+  end
+
+  def destroy 
+  end   
+
+
+private 
+
+def comment_params
+  params.require(:comment).permit(:post, :user, :comment)
+end
+
 end
