ed1d3f29aafbdb6a01804a138c954051989e0091
josephrfarmer <josephr.farmer24@gmail.com>
2018-08-31 11:01:46 -0400

    commiting to pull and merge development

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,9 +14,17 @@ class CommentsController < ApplicationController
   end
 
   def edit 
+    @comment = Comment.find(params[:id])
   end  
 
   def update 
+    @comment = Comment.find(params[:id])
+
+    if @comment.update(comment_params)
+      redirect_to post_path, notice: 'Comment was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy 
