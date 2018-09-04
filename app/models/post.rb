f6fcd6b1e5c90fd19e2ad676799fc9711017813c
Christopher Dean Hagmann <cdhagmann@gmail.com>
2018-09-04 10:06:21 -0400

    Added Post validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ActiveRecord::Base
   has_many :upvotes, dependent: :destroy
   has_many :comments, dependent: :destroy
   belongs_to :user
+  validates :title, :link, presence: true
   
   def score
     upvotes.count
