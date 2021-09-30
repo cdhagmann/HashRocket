f88817ec9c510f8273b22f227198ae7edc440a0d
Christopher Hagmann <chris.hagmann@scimedsolutions.com>
2021-09-30 17:35:29 -0400

    Update to Rails 6

---
+++ app/javascript/application.js
@@ -0,0 +1,6 @@
+// Entry point for the build script in your package.json
+import * as bootstrap from "bootstrap"
+import * as fontawesome from "@fortawesome/fontawesome-free"
+import Rails from '@rails/ujs';
+
+Rails.start();
