161869bc1f8aee66e2e4f6089b164d48ea109093
Christopher Hagmann <chris.hagmann@scimedsolutions.com>
2021-09-30 19:37:10 -0400

    Add fa_icon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,12 @@
 module ApplicationHelper
+  include ActionView::Helpers::Tags
+  include ActionView::Helpers::TagHelper
+
   def timestamp_to_words(timestamp)
     "#{time_ago_in_words(timestamp).gsub('about', '')} ago"
+  end
+
+  def fa_icon(icon)
+    tag.i(class: icon)
   end
 end
